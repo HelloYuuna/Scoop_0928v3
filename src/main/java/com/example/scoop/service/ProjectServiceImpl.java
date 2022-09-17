320fa2dc3a6e73a1b8177227676f825b462129f4
scoop <hhs8028@naver.com>
2022-09-17 18:14:01 +0900

    살려줘

---
+++ src/main/java/com/example/scoop/service/ProjectServiceImpl.java
@@ -0,0 +1,108 @@
+package com.example.scoop.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.scoop.dao.ProjectDAO;
+import com.example.scoop.domain.Project;
+import com.example.scoop.domain.User;
+import com.example.scoop.domain.Project;
+
+@Service
+public class ProjectServiceImpl implements ProjectService{
+	
+	@Autowired
+	private ProjectDAO projectdao;
+	
+//		public String updateContent(Project project) {
+//			
+//			String result = projectdao.updateContent(project);
+//			
+//			return result;
+//		}
+		
+//		public ArrayList<Project> selectContent(){
+//			
+//			ArrayList<Project> list = projectdao.selectContent();
+//			
+//			// if(project.getCount() == ' ') {
+//			//	 list.setCount('');
+//			// }
+//			return list;
+//		}
+
+		@Override
+		public int updateContent(Project pproject) {
+			int result = projectdao.updateContent(pproject);
+			
+			return result;
+		}
+
+		@Override
+		public String selectContent(int pnum) {
+			String result = projectdao.selectContent(pnum);
+			
+			return result;
+		}
+		
+		@Override
+		public ArrayList<User> selectMember(){
+			
+			ArrayList<User> result = projectdao.selectMember();
+			return result;
+			
+		}
+		
+		public String memberselect(String membername) {
+			
+			String result = projectdao.memberselect(membername);
+			return result;
+		}
+		
+		public int projectInsert(Project pproject) {
+			
+			int result = projectdao.projectInsert(pproject);
+			
+			return result;
+		}
+		
+		public ArrayList<Project> selectProject(){
+			
+			ArrayList<Project> result = projectdao.selectProject();
+			return result;
+			
+		}
+		
+		public Project projectread(int pnum) {
+			
+			Project pproject = projectdao.projectread(pnum);
+			
+			return pproject;
+		}
+		
+		public int updatemember(int pnum, User user) {
+			
+			//select String으로 pmember 다 받아오고
+			String pmember = choiceMember(pnum);
+			pmember += user.getName();
+			//map을 선언한다. pnum과 user를 넘기는 
+			HashMap<String, Object> map = new HashMap<>();
+			
+			map.put("pmember", pmember);
+			
+			int result = projectdao.updatemember(map);
+			
+			return result;	
+		}
+		
+		
+		public String choiceMember(int pnum) {
+			
+			String result = projectdao.choiceMember(pnum);
+			return result;
+		}
+		
+}
