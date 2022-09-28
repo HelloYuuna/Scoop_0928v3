a05c46c85fcb77fd033fe0ab1066629423b0eb06
wonsiin <answngusdld@gmail.com>
2022-09-28 21:04:21 +0900

    "09-28"

--- src/main/java/com/example/scoop/service/GoalSeviceImpl.java
+++ src/main/java/com/example/scoop/service/GoalSeviceImpl.java
@@ -25,45 +25,45 @@ public class GoalSeviceImpl implements GoalService {
         return result;
     };
 
-    public ArrayList<Goal> selectOne1(Goal goal0) {
-        ArrayList<Goal> result = boardDAO.selectOne1(goal0);
+    public ArrayList<Goal> selectOne1(Goal goal) {
+        ArrayList<Goal> result = boardDAO.selectOne1(goal);
 
-        for (Goal goal : result) {
-            log.debug("처음값 : {}", goal);
-            if (goal.getGstartdate() != "" && goal.getGenddate() != "" &&
-                    goal.getGstartdate() != null && goal.getGenddate() != null) {
-                String goal1 = goal.getGstartdate() + "~" + goal.getGenddate();
-                goal.setCalendar(goal1);
-                log.debug("Calendar1 : {}", goal);
-            } else if (goal.getGstartdate() == "" || goal.getGenddate() == "" &&
-                    goal.getGstartdate() == null || goal.getGenddate() == null) {
-                String goal1 = "";
-                goal.setCalendar(goal1);
+        for (Goal goal1 : result) {
+            log.debug("처음값 : {}", goal1);
+            if (goal1.getGstartdate() != "" && goal1.getGenddate() != "" &&
+                    goal1.getGstartdate() != null && goal1.getGenddate() != null) {
+                String goal2 = goal1.getGstartdate() + "~" + goal1.getGenddate();
+                goal1.setCalendar(goal2);
+                log.debug("Calendar1 : {}", goal1);
+            } else if (goal1.getGstartdate() == "" || goal1.getGenddate() == "" &&
+                    goal1.getGstartdate() == null || goal1.getGenddate() == null) {
+                String goal2 = "";
+                goal.setCalendar(goal2);
                 log.debug("Calendar2 : {}", goal);
             }
-            if (goal.getGstatus() == 0) {
-                goal.setGstatus1("진행중");
+            if (goal1.getGstatus() == 0) {
+                goal1.setGstatus1("진행중");
             }
-            if (goal.getGstatus() == 1) {
-                goal.setGstatus1("위험");
+            if (goal1.getGstatus() == 1) {
+                goal1.setGstatus1("위험");
             }
-            if (goal.getGstatus() == 2) {
-                goal.setGstatus1("기간지남");
+            if (goal1.getGstatus() == 2) {
+                goal1.setGstatus1("기간지남");
             }
-            if (goal.getGstatus() == 3) {
-                goal.setGstatus1("완료");
+            if (goal1.getGstatus() == 3) {
+                goal1.setGstatus1("완료");
             }
-            if (goal.getGstatus() == 4) {
-                goal.setGstatus1("보류");
+            if (goal1.getGstatus() == 4) {
+                goal1.setGstatus1("보류");
             }
-            if (goal.getGstatus() == 5) {
-                goal.setGstatus1("목표완성");
+            if (goal1.getGstatus() == 5) {
+                goal1.setGstatus1("목표완성");
             }
-            if (goal.getGstatus() == 6) {
-                goal.setGstatus1("목표미완성");
+            if (goal1.getGstatus() == 6) {
+                goal1.setGstatus1("목표미완성");
             }
-            if (goal.getGstatus() == 7) {
-                goal.setGstatus1("목표중지");
+            if (goal1.getGstatus() == 7) {
+                goal1.setGstatus1("목표중지");
             }
         }
         // 이걸 적으니 2개씩 출력됨
